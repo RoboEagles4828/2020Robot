71a134bf4c7d3536ea64557017f42a4cd29cde9b
Jackson McClintock <jacksondm33@gmail.com>
2020-02-18 17:15:55 -0500

    Add frame width variable

--- pi/main.py
+++ pi/main.py
@@ -17,14 +17,14 @@ class Blocks(Structure):
                 ("m_index", c_uint), ("m_age", c_uint)]
 
 
+frame_width = 315
 blocks = BlockArray(100)
 
 while 1:
     count = pixy.ccc_get_blocks(100, blocks)
 
     if count == 1:
-        network_table.putNumber("value",
-                                2 * blocks[0].m_x / pixy.frameWidth - 1)
+        network_table.putNumber("value", 2 * blocks[0].m_x / frame_width - 1)
         print('[BLOCK: SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' %
               (blocks[0].m_signature, blocks[0].m_x, blocks[0].m_y,
                blocks[0].m_width, blocks[0].m_height))
