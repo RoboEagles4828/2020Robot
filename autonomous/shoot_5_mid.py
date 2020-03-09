990aa7831f57e23aa228bc79d21ca064ef02b692
Tommy Smith <neptommy@gmail.com>
2020-03-09 11:04:06 -0400

    no comment

--- autonomous/shoot_5_mid.py
+++ autonomous/shoot_5_mid.py
@@ -2,6 +2,7 @@ from robotpy_ext.autonomous import StatefulAutonomous
 from robotpy_ext.autonomous.stateful_autonomous import state
 from robotpy_ext.autonomous.stateful_autonomous import timed_state
 from navx import AHRS
+from networktables import NetworkTable
 
 import config
 from autonomous.autonomous import Autonomous
@@ -15,10 +16,67 @@ class Shoot5Mid(StatefulAutonomous):
     drivetrain: Drivetrain
     navx: AHRS
     shooter: Shooter
+    nt_pi: NetworkTable
 
     MODE_NAME = "Shoot 5 Mid"
 
-    @timed_state(duration=3.0, next_state="drive1", first=True)
+    @timed_state(duration=3.0, next_state="turn1", first=True)
+    def shoot1(self):
+        self.autonomous.shoot_0()
+
+    @state
+    def turn1(self, initial_call):
+        if self.autonomous.turn(initial_call, -10):
+            self.next_state("drive1")
+    @state
+    def drive1(self, initial_call):
+        if self.autonomous.dirve(initial_call, 130):
+            self.next_state("turn2")
+
+    @state
+    def turn2(self, initial_call):
+        if self.autonomous.turn(initial_call, 110):
+            self.next_state("drive2")
+
+    @state
+    def drive2(self, initial_call):
+        self.shooter.set_intake_speed(config.Robot.Shooter.INTAKE_SPEED)
+        if self.autonomous.drive(initial_call, 24):
+            self.next_state("drive3")
+
+    @state
+    def drive3(self, initial_call):
+        self.shooter.set_intake_speed(0)
+        if self.autonomous.drive(initial_call, -24):
+            self.next_state("turn3")
+
+    @state
+    def turn3(self, initial_call):
+        if self.autonomous.turn(initial_call, -110):
+            self.next_state("drive4")
+
+    @state
+    def drive4(self, initial_call):
+        if self.autonomous.drive(initial_call, -130):
+            self.next_state("vision1")
+
+    @timed_state(duration=1.0, next_state="shoot2", first=True)
+    def vision1(self):
+        value = self.nt_pi.getNumber("value",
+                                     0) * config.Robot.Drivetrain.VISION_RATIO
+        if abs(value) < 0.05:
+            value = value / abs(value) * 0.05
+        self.drivetrain.set_speeds(value, -value)
+
+    @timed_state(duration=3.0, next_state="end")
+    def shoot2(self):
+        self.autonomous.shoot_0()
+
+    @state
+    def end(self):
+        self.drivetrain.set_speeds(0,0)
+        self.done()
+    """@timed_state(duration=3.0, next_state="drive1", first=True)
     def shoot1(self):
         self.autonomous.shoot_0()
 
@@ -72,4 +130,4 @@ class Shoot5Mid(StatefulAutonomous):
     @state
     def end(self):
         self.shooter.set_shooter_speed(0)
-        self.done()
+        self.done()"""
